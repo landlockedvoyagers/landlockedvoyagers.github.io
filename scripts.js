0aaf6872de3d04803089330ad957ea09d1644607
John Miller <landlockedvoyagers@gmail.com>
2024-06-20 23:32:10 -0600

    GOOOO

--- scripts.js
+++ scripts.js
@@ -7,3 +7,13 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         });
     });
 });
+
+// scripts.js
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    if (window.scrollY > 100) {
+        footer.style.display = 'none';
+    } else {
+        footer.style.display = 'block';
+    }
+});
